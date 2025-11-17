61758429d2b2a3a84e63445ee4d51c99c1ec9875
Coded76 <0xhope66@gmail.com>
2025-11-17 17:54:30 +0100

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,27 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="max-w-7xl mx-auto flex pt-10 items-center justify-between py-4 px-6">
-      <div className="flex items-center gap-1.5">
+    <div className="max-w-7xl mx-auto flex pt-6 md:pt-10 items-center justify-between py-3 md:py-4 px-4 md:px-6">
+      <div className="flex items-center gap-1 md:gap-1.5">
         <Image
           src="/assets/logo.png"
           alt="Duel Token Logo"
           width={50}
           height={50}
+          className="w-8 h-8 md:w-12 md:h-12"
         />
-        <h2 className="text-[36px] font-bold text-white">DUEL</h2>
+        <h2 className="text-[20px] md:text-[36px] font-bold text-white">DUEL</h2>
         <Image
           src="/assets/sword.png"
           alt="Duel Token Logo"
           width={50}
           height={50}
+          className="w-8 h-8 md:w-12 md:h-12"
         />
       </div>
       <div>
-        <button className="relative w-[166px] h-[55px] text-white text-[24px] font-medium rounded-[45px] p-0.5 cursor-pointer hover:scale-95 duration-300 transition-all bg-linear-to-r from-[#32F0FF] to-[#E239FB]">
-          <span className="flex items-center justify-center w-full h-full rounded-[40px] bg-[#040a1c]">
+        <button className="relative w-[120px] md:w-[166px] h-[48px] md:h-[55px] text-white text-[14px] md:text-[24px] font-medium rounded-full p-0.5 cursor-pointer hover:scale-95 duration-300 transition-all bg-gradient-to-r from-[#32F0FF] to-[#E239FB]">
+          <span className="flex items-center justify-center w-full h-full rounded-full bg-[#040a1c]">
             Get $DUEL
           </span>
         </button>
