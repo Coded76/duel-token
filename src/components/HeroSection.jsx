11f8962f1b5eaf0abcbe4cc4824919005a356563
Coded76 <0xhope66@gmail.com>
2025-11-17 16:50:33 +0100

    updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,18 +10,18 @@ function HeroSection() {
     >
       <Navbar />
 
-      <div className="flex md:flex-row flex-col md:pt-[60px] pt-[40px] max-w-7xl mx-auto items-center justify-between px-16 md:px-6 gap-10">
+      <div className="flex md:flex-row flex-col md:pt-[60px] pt-[40px] max-w-7xl mx-auto items-center justify-between px-16 md:px-6 gap-6 md:gap-10">
         {/* LEFT CONTENT */}
-        <div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left">
-          <h2 className="text-[#1EF9F9] text-[24px] md:text-[40px] font-bold">
+        <div className="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left gap-3 md:gap-4">
+          <h2 className="text-[#1EF9F9] text-[20px] md:text-[40px] font-bold">
             DUEL.COM
           </h2>
 
-          <h2 className="text-white text-[24px] md:text-[40px] pt-[18px] font-bold md:max-w-[461px] leading-tight">
+          <h2 className="text-white text-[28px] md:text-[40px] font-bold md:max-w-[461px] leading-tight">
             The First Casino That Gives a Fuck.
           </h2>
 
-          <p className="text-white pt-[14px] text-[16px] font-bold md:max-w-[461px]">
+          <p className="text-white text-[14px] md:text-[16px] font-bold md:max-w-[461px]">
             Just the purest form of gambling — The way it should be.
           </p>
 
@@ -51,11 +51,11 @@ function HeroSection() {
             {" "}
             <div className="flex items-center gap-2.5">
               {" "}
-              <button className="md:w-[205px] w-[130px] h-[61px] bg-[#13223A] md:text-[20px] text-[12px] font-bold cursor-pointer hover:scale-95 duration-300 transition-all border border-[#0759FF] rounded-xl">
+              <button className="md:w-[205px] w-[150px] text-white h-[61px] bg-[#13223A] md:text-[20px] text-[12px] font-bold cursor-pointer hover:scale-95 duration-300 transition-all border border-[#0759FF] rounded-xl">
                 {" "}
                 Go to <span className="text-[#00A6FF]">DUEL.COM</span>{" "}
               </button>{" "}
-              <button className="md:w-[205px] w-[130px] h-[61px] bg-[#13223A] md:text-[20px] text-[12px] font-bold cursor-pointer hover:scale-95 duration-300 transition-all border border-[#0759FF] rounded-xl">
+              <button className="md:w-[205px] w-[150px] text-white h-[61px] bg-[#13223A] md:text-[20px] text-[12px] font-bold cursor-pointer hover:scale-95 duration-300 transition-all border border-[#0759FF] rounded-xl">
                 {" "}
                 Whitepaper{" "}
               </button>{" "}
